a6663fee7e0817909afe028935881ff1f6a536f0
Ponseca,Stephen Matthew <113017689+Stephenmatthewponseca@users.noreply.github.com>
2022-10-12 22:21:27 +0800

    Add files via upload

---
+++ demo(3).php
@@ -0,0 +1,21 @@
+//Grade_Shit
+<html>
+<body>
+
+			echo "<br><br>";
+			$name = "sage";
+			$grade = 59;
+			if ($grade >= 70) 
+			{
+				echo "Name: ".$name . "<br>Grade: ". $grade . ", you passed. <br>";
+			} 
+			elseif (($grade >=60) && ($grade <=69))
+			{
+				echo "Name: ".$name . "<br>Grade: ".$grade . ", get remedial exam. <br>";
+			}
+			else
+			{
+				echo "Name: ".$name . "<br>Grade: ".$grade . ", you failed. <br>";
+			}
+			</html>
+			</body>
